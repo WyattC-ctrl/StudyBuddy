0b31967c54ce3c2c517e5e38046faaebed479d69
Aayan Hussain <aayanhussainw07@gmail.com>
2025-11-24 18:50:32 -0500

    db additions

--- backend/db.py
+++ backend/db.py
@@ -4,7 +4,7 @@ db = SQLAlchemy()
 
 class User(db.Model):
    """
-   Model depicting a user.
+   Depicts a User.
    """
    __tablename__ = "users"
    id = db.Column(db.Integer, primary_key=True)
@@ -12,13 +12,23 @@ class User(db.Model):
    email = db.Column(db.String(100), nullable=False)
    password = db.Column(db.String(100), nullable=False)
    profile_id = db.Column(db.Integer, db.ForeignKey("profiles.id", ondelete="SET NULL"), unique=True, nullable=True)
-   
+
 class Profile(db.Model):
    """
-   Model depicting a profile.
+   Depicts a Profile.
    """
    __tablename__ = "profiles"
    id = db.Column(db.Integer, primary_key=True)
-   bio = db.Column(db.String(255), nullable=True)
-   # user_id links back to the owning user
    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False, unique=True)
+   description = db.Column(db.String(255), nullable=True)
+   major = db.Column(db.String(255), nullable=True)
+   studytime = ???
+   courses = ???
+   
+class Courses(db.Model):
+   """
+   Depicts a Course.
+   """
+   id = db.Column(db.Integer, primary_key=True)
+   code = db.Column(db.String(100), nullable=False)
+   profiles = ???
